b61fb12547a3f825bf505accec20ba085abcf36e
abdulwahidansari <abdulwahidansari98@gmail.com>
2023-07-17 04:22:25 +0500

    first commit

---
+++ src/components/layout/Header.tsx
@@ -0,0 +1,29 @@
+import Link from "next/link"
+import Logo from "/public/Logo.webp"
+import Image from 'next/image'
+import {ShoppingCart} from "lucide-react"
+
+const Header = () => {
+  return (
+    <div className="flex justify-between items-center px-8 py-6">
+      <Image src={Logo} alt="" />
+      <ul className="flex gap-x-10">
+        <li className="text-lg"><Link href={"/ "}>Female </Link></li>
+        <li className="text-lg"><Link href={"/ "}>Male </Link></li>
+        <li className="text-lg"><Link href={"/ "}>Kids </Link></li>
+        <li className="text-lg"><Link href={"/ "}>All Product </Link></li>
+        </ul>
+
+        <div className="h-10 w-10 rounded-full bg-gray-300 flex justify-center items-center" >
+          <ShoppingCart/>
+
+
+        </div>
+
+
+    
+    </div>
+  )
+}
+
+export default Header
